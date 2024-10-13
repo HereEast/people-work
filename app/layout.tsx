c3b238982f8971c223f57c5ffb49f27dd122187a
Nastia Piven <nastiapiven@gmail.com>
2024-10-13 21:27:38 +0200

    fix: fix paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,22 +8,22 @@ import localFont from "next/font/local";
 const InnovatorGrotesk = localFont({
   src: [
     {
-      path: "./fonts/InnovatorGrotesk-Regular.woff2",
+      path: "/fonts/InnovatorGrotesk-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/InnovatorGrotesk-Medium.woff2",
+      path: "/fonts/InnovatorGrotesk-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/InnovatorGrotesk-Semibold.woff2",
+      path: "/fonts/InnovatorGrotesk-Semibold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./fonts/InnovatorGrotesk-Bold.woff2",
+      path: "/fonts/InnovatorGrotesk-Bold.woff2",
       weight: "700",
       style: "normal",
     },
