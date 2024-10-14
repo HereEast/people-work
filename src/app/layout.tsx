85e269a5fce36a86ca90f9e509454821970f5dcd
Nastia Piven <nastiapiven@gmail.com>
2024-10-14 21:16:48 +0200

    feat: rename font files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,22 +8,22 @@ import localFont from "next/font/local";
 const InnovatorGrotesk = localFont({
   src: [
     {
-      path: "../../public/fonts/InnovatorGrotesk-Regular.woff2",
+      path: "../../public/fonts/innovator-grotesk-regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/InnovatorGrotesk-Medium.woff2",
+      path: "../../public/fonts/innovator-grotesk-medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/fonts/InnovatorGrotesk-Semibold.woff2",
+      path: "../../public/fonts/innovator-grotesk-semibold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../public/fonts/InnovatorGrotesk-Bold.woff2",
+      path: "../../public/fonts/innovator-grotesk-bold.woff2",
       weight: "700",
       style: "normal",
     },
