9cea54c70825d2581f7af5a69e43699362312011
Nastia Piven <nastiapiven@gmail.com>
2024-10-14 22:23:14 +0200

    fix: wip

--- src/components/layouts/Home.tsx
+++ src/components/layouts/Home.tsx
@@ -9,9 +9,12 @@ import { IPerson } from "~/utils/types";
 
 export function Home() {
   const [person, setPerson] = useState<IPerson | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchPerson() {
+      setIsLoading(true);
+
       try {
         const data = await getPerson("margo-lazarenkova");
 
@@ -20,6 +23,8 @@ export function Home() {
         }
       } catch (err) {
         console.log(err);
+      } finally {
+        setIsLoading(false);
       }
     }
 
@@ -30,41 +35,50 @@ export function Home() {
     ? person.companyURL
     : `https://${person?.companyURL}`;
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <section className="flex items-center justify-center gap-8 sm:items-start">
-      <div className="flex flex-col items-center space-y-2">
-        <ImageThumbnail
-          src={`/images/people/${person?.profileImageURL}` || ""}
-          alt={person?.name || ""}
-        />
+      {person && (
+        <div className="flex flex-col items-center space-y-2">
+          <ImageThumbnail
+            src={`/images/people/${person?.profileImageURL}` || ""}
+            alt={person?.name || ""}
+          />
 
-        <div className="mb-2">
-          <h4 className="hover:opacity-50">
-            <Link href={`/people/${person?.slug}`} className="space-y-[-10px]">
-              {person?.name.split(" ").map((item) => (
-                <span
-                  className="leading-0 block text-center text-5xl font-bold tracking-tighter"
-                  key={item}
-                >
-                  {item}
-                </span>
-              ))}
-            </Link>
-          </h4>
-        </div>
+          <div className="mb-2">
+            <h4 className="hover:opacity-50">
+              <Link
+                href={`/people/${person?.slug}`}
+                className="space-y-[-10px]"
+              >
+                {person?.name.split(" ").map((item) => (
+                  <span
+                    className="leading-0 block text-center text-5xl font-bold tracking-tighter"
+                    key={item}
+                  >
+                    {item}
+                  </span>
+                ))}
+              </Link>
+            </h4>
+          </div>
 
-        <div className="flex flex-col items-center space-y-[-5px]">
-          <a
-            href={companyURL}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="leading-0 underline hover:no-underline hover:opacity-50"
-          >
-            {person?.company}
-          </a>
-          <p className="leading-0">{person?.jobTitle}</p>
+          <div className="flex flex-col items-center space-y-[-5px]">
+            <a
+              href={companyURL}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="leading-0 underline hover:no-underline hover:opacity-50"
+            >
+              {person?.company}
+            </a>
+            <p className="leading-0">{person?.jobTitle}</p>
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
 }
