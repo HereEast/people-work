f2a13a25adbd1901cd1df1159396facc21029f51
Nastia Piven <nastiapiven@gmail.com>
2025-02-03 16:59:21 +0100

    fix: fx logo hover shadows

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function Logo() {
   return (
-    <div className="flex h-8 items-center justify-between overflow-hidden rounded-full bg-stone-950 pl-3 pr-2 transition hover:shadow-lg hover:shadow-blue-600 sm:gap-10">
+    <div className="flex h-8 items-center justify-between overflow-hidden rounded-full bg-stone-950 pl-3 pr-2 sm:gap-10">
       <h1 className="mb-1 text-nowrap text-2xl font-bold text-stone-50">
         people—work.net
       </h1>
