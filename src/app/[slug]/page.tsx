616d5b168c6ae414bb8fd1b22cb25ee5a3c9db89
Nastia Piven <nastiapiven@gmail.com>
2025-02-05 13:50:22 +0100

    feat: update metadata

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { QAPage } from "~/components/layouts/QAPage";
 
 import { connectDB } from "../lib/connectDB";
 import { Person, IPerson } from "~/models/Person";
+import { OG } from "~/utils/constants";
 
 interface PersonPageProps {
   params: {
@@ -15,15 +16,38 @@ export async function generateMetadata({ params }: PersonPageProps) {
   const person: IPerson = await Person.findOne({ slug: params.slug }).exec();
 
   if (person) {
-    const title = `PEOPLE—WORK.NET | ${person.name}, ${person.jobTitle} at ${person.company.name}`;
+    const title = `${person.name}, ${person.jobTitle} at ${person.company.name}`;
 
     return {
-      title: title,
+      title,
+      description: OG.DESCRIPTION,
+      metadataBase: new URL(OG.BASE_URL),
       openGraph: {
-        title: title,
+        title,
+        description: OG.DESCRIPTION,
+        url: OG.BASE_URL,
+        siteName: "people-work.net",
+        images: [
+          {
+            url: OG.IMAGE,
+            width: 1200,
+            height: 630,
+          },
+        ],
+        locale: "en-EN",
       },
       twitter: {
-        title: title,
+        title,
+        description: OG.DESCRIPTION,
+        site: "people-work.net",
+        card: "summary_large_image",
+        images: [
+          {
+            url: OG.IMAGE,
+            width: 1200,
+            height: 630,
+          },
+        ],
       },
     };
   }
