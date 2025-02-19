171c5630355bcb88f55a73b96f14e5923d7b4249
Nastia Piven <nastiapiven@gmail.com>
2025-02-19 21:04:00 +0100

    refactor: move font to fonts.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import { Metadata } from "next";
 
-import localFont from "next/font/local";
 import "./globals.css";
 
 import { ClientProvider } from "~/providers/ClientProvider";
@@ -12,41 +11,7 @@ import { GoogleAnalytics } from "~/components/GoogleAnalytics";
 import { CookieBanner } from "~/components/CookieBanner";
 
 import { getMetadata } from "~/utils/getMetadata";
-
-const InnovatorGrotesk = localFont({
-  src: [
-    {
-      path: "../../public/fonts/innovator-grotesk-regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/innovator-grotesk-medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/innovator-grotesk-semibold.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/innovator-grotesk-bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/innovator-grotesk-extrabold.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/innovator-grotesk-black.woff2",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-});
+import { InnovatorGrotesk } from "~/utils/fonts";
 
 export const metadata: Metadata = getMetadata();
 
---
+++ src/utils/fonts.ts
@@ -0,0 +1,36 @@
+import localFont from "next/font/local";
+
+export const InnovatorGrotesk = localFont({
+  src: [
+    {
+      path: "../../public/fonts/innovator-grotesk-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/innovator-grotesk-medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/innovator-grotesk-semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/innovator-grotesk-bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/innovator-grotesk-extrabold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/innovator-grotesk-black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
