bb212bdfb71ef67f8f9d1cd0f0e3850a7dd36a9d
Nastia Piven <nastiapiven@gmail.com>
2025-03-24 19:01:42 +0100

    refactor: rearrange fonts

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -9,32 +9,32 @@ export const Inter = InterFont({
 export const InnovatorGrotesk = localFont({
   src: [
     {
-      path: "../../public/fonts/innovator-grotesk-regular.woff2",
+      path: "../app/fonts/innovator-grotesk-regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/innovator-grotesk-medium.woff2",
+      path: "../app/fonts/innovator-grotesk-medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/fonts/innovator-grotesk-semibold.woff2",
+      path: "../app/fonts/innovator-grotesk-semibold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../public/fonts/innovator-grotesk-bold.woff2",
+      path: "../app/fonts/innovator-grotesk-bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../public/fonts/innovator-grotesk-extrabold.woff2",
+      path: "../app/fonts/innovator-grotesk-extrabold.woff2",
       weight: "800",
       style: "normal",
     },
     {
-      path: "../../public/fonts/innovator-grotesk-black.woff2",
+      path: "../app/fonts/innovator-grotesk-black.woff2",
       weight: "900",
       style: "normal",
     },
