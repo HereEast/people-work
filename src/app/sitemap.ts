c931df1c12ec3655bf077c0295d8fcf2376dc4ee
Nastia Piven <nastiapiven@gmail.com>
2025-04-30 21:43:33 +0200

    fix: fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,30 +1,25 @@
 import type { MetadataRoute } from "next";
 
+import { getQuestions } from "~/api-client/questions";
+import { getPeople } from "~/api-client/people";
 import { BASE_URL } from "~/utils/constants";
-import { connectDB } from "../lib/connectDB";
-import { PersonDB } from "~/models/Person";
-import { QuestionDB } from "~/models/Question";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  await connectDB();
-
-  const people = await PersonDB.find({ isActive: true }).exec();
-  const questions = await QuestionDB.find({
-    isActive: true,
-  }).exec();
+  const people = await getPeople();
+  const questions = await getQuestions();
 
   const peopleSitemapData =
     people?.map((person) => {
       return {
-        url: `${BASE_URL}/${person.slug}`,
-        lastModified: person.updatedAt,
+        url: `${BASE_URL}/people/${person.slug}`,
+        lastModified: new Date(),
       };
     }) || [];
 
   const questionsSitemapData =
     questions?.map((question) => {
       return {
-        url: `${BASE_URL}/${question.slug}`,
+        url: `${BASE_URL}/questions/${question.slug}`,
         lastModified: new Date(),
       };
     }) || [];
@@ -35,7 +30,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       lastModified: new Date(),
     },
     {
-      url: "/questions",
+      url: `${BASE_URL}/questions`,
       lastModified: new Date(),
     },
     ...peopleSitemapData,
