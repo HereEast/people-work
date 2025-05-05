b648e41cddf600b112a958b713f1504940d4db10
Nastia Piven <nastiapiven@gmail.com>
2025-05-05 22:28:26 +0200

    feat: add api endpoint

---
+++ src/app/api/answers/featured/[id]/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+
+import { connectDB } from "~/lib/connectDB";
+import { AnswerDB, IAnswerDB } from "~/models/Answer";
+import { DBDoc } from "~/utils/types";
+import { mapAnswerBasicData } from "~/utils/mappers";
+
+interface ReqParams {
+  params: { id: string };
+}
+
+// GET ANSWERS BY SLUG
+export async function GET(req: Request, { params }: ReqParams) {
+  const { id } = params;
+
+  try {
+    await connectDB();
+
+    const doc: DBDoc<IAnswerDB> | null = await AnswerDB.findOne({
+      personId: id,
+      featured: true,
+    }).exec();
+
+    if (!doc) {
+      return NextResponse.json(
+        {
+          message: `🔴 Featured answer for Person ID ${id} is not found.`,
+        },
+        {
+          status: 404,
+        },
+      );
+    }
+
+    const answer = mapAnswerBasicData(doc);
+
+    return NextResponse.json(answer);
+  } catch (err) {
+    console.log(err);
+
+    return NextResponse.json(
+      { message: "🔴 Error fetching a featured answer." },
+      {
+        status: 500,
+      },
+    );
+  }
+}
