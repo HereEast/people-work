500d5c9b28b7160f670714c4a9ff2efd760170bf
Nastia Piven <nastiapiven@gmail.com>
2025-05-06 18:31:28 +0200

    test: move back

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,13 +8,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const people = await getPeople();
   const questions = await getQuestions();
 
-  // const peopleSitemapData =
-  //   people?.map((person) => {
-  //     return {
-  //       url: `${BASE_URL}/people/${person.slug}`,
-  //       lastModified: new Date(),
-  //     };
-  //   }) || [];
+  const peopleSitemapData =
+    people?.map((person) => {
+      return {
+        url: `${BASE_URL}/people/${person.slug}`,
+        lastModified: new Date(),
+      };
+    }) || [];
 
   const questionsSitemapData =
     questions?.map((question) => {
@@ -33,7 +33,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: `${BASE_URL}/questions`,
       lastModified: new Date(),
     },
-    // ...peopleSitemapData,
+    ...peopleSitemapData,
     ...questionsSitemapData,
   ];
 }
