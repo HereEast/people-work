6321c1b5b20e98c5fde3655f22b3324be3ffa4fd
Nastia Piven <nastiapiven@gmail.com>
2025-05-07 10:32:14 +0200

    test

--- src/utils/handlers.ts
+++ src/utils/handlers.ts
@@ -4,16 +4,18 @@ import { ClassValue, clsx } from "clsx";
 import { featuredSlugs } from "./data/featured";
 
 // Base URL
-export function getBaseUrl() {
-  if (typeof window !== "undefined") return "";
+// export function getBaseUrl() {
+//   if (typeof window !== "undefined") {
+//     return "";
+//   }
 
-  if (process.env.VERCEL_URL) {
-    const url = process.env.VERCEL_URL;
-    return url.startsWith("localhost") ? `http://${url}` : `https://${url}`;
-  }
+//   if (process.env.VERCEL_URL) {
+//     const url = process.env.VERCEL_URL;
+//     return url.startsWith("localhost") ? `http://${url}` : `https://${url}`;
+//   }
 
-  return "http://localhost:3000";
-}
+//   return "http://localhost:3000";
+// }
 
 // Get random slugs
 export function getRandomSlugs() {
