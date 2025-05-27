f8a1295d79316e6ec393035fd0eed9f50a8a9e92
Nastia Piven <nastiapiven@gmail.com>
2025-05-27 18:21:50 +0200

    feat: update CookieBanner

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCookie } from "~/hooks/useCookie";
+import { Button } from "./ui/Button";
 
 export function CookieBanner() {
   const { isCookieBanner, setConsent } = useCookie();
@@ -10,26 +11,22 @@ export function CookieBanner() {
   }
 
   return (
-    <div className="fixed inset-x-2 bottom-2 z-30 mx-auto sm:inset-x-4 sm:bottom-6">
-      <div className="flex flex-col items-center justify-between gap-4 rounded-xl p-6 text-stone-50 md:flex-row">
-        <div className="w-full text-center md:text-left">
-          <p>🍪 We use cookies on our website.</p>
-        </div>
+    <div className="fixed inset-x-2.5 bottom-2.5 z-30 max-w-[620px] sm:bottom-6 sm:m-auto">
+      <div className="flex flex-col items-center justify-between gap-2 rounded-md bg-stone-300 p-4 pt-3 text-lg sm:flex-row sm:p-4 sm:pl-5 sm:text-xl">
+        <p>🍪 This website uses Cookies.</p>
 
-        <div className="flex w-full justify-center gap-2 md:justify-end">
-          <button
+        <div className="flex w-full gap-2 sm:w-fit">
+          <Button
+            view="outline"
             onClick={() => setConsent(false)}
-            className="w-full rounded-[16px] border border-stone-50 px-10 py-4 transition hover:opacity-75 md:w-fit"
+            className="w-full sm:w-fit"
           >
             Decline
-          </button>
+          </Button>
 
-          <button
-            onClick={() => setConsent(true)}
-            className="w-full rounded-[16px] bg-stone-50 px-10 py-4 font-medium text-stone-900 transition hover:opacity-75 md:w-fit"
-          >
+          <Button onClick={() => setConsent(true)} className="w-full sm:w-fit">
             Accept
-          </button>
+          </Button>
         </div>
       </div>
     </div>
