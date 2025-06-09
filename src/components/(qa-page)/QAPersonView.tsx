55db95dc0fc8f437f474836737483c891ed40abb
Nastia Piven <nastiapiven@gmail.com>
2025-06-09 19:58:10 +0200

    fix: comment schema

--- src/components/(qa-page)/QAPersonView.tsx
+++ src/components/(qa-page)/QAPersonView.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { AccentText } from "~/components/AccentText";
 import { StickyMobileWrapper } from "~/components/Card";
@@ -6,7 +7,6 @@ import { Column } from "~/components/Column";
 import { LinkedinButton } from "~/components/Buttons";
 
 import { PersonData } from "~/schemas";
-import Image from "next/image";
 
 interface PersonViewProps {
   person: PersonData;
@@ -16,7 +16,7 @@ export function QAPersonView({ person }: PersonViewProps) {
   return (
     <>
       <DesktopPersonView person={person} />
-      {/* <MobilePersonView person={person} /> */}
+      <MobilePersonView person={person} />
     </>
   );
 }
@@ -107,38 +107,45 @@ function DesktopPersonView({ person }: PersonViewProps) {
 }
 
 // Mobile
-// function MobilePersonView({ person }: PersonViewProps) {
-//   return (
-//     <StickyMobileWrapper>
-//       <div className="flex items-end gap-2.5 py-3 sm:gap-4 sm:py-5">
-//         <Image
-//           src={`/images/people/${person.slug}.jpg`}
-//           alt={`Image of ${person.name}`}
-//           width={600}
-//           height={600}
-//           className="mb-px size-[44px] rounded-xs object-cover sm:size-[68px]"
-//           priority
-//         />
+function MobilePersonView({ person }: PersonViewProps) {
+  return (
+    <StickyMobileWrapper>
+      <div className="flex items-end gap-2.5 py-3 sm:gap-4 sm:py-5">
+        <Image
+          src={`/images/people/${person.slug}.jpg`}
+          alt={`Image of ${person.name}`}
+          width={600}
+          height={600}
+          className="mb-px size-[44px] rounded-xs object-cover sm:size-[68px]"
+          priority
+        />
 
-//         <div className="flex flex-col gap-px text-xl text-stone-50 sm:text-3xl">
-//           <h1 className="leading-none">
-//             <AccentText>{person.name}</AccentText>
-//           </h1>
+        <div className="flex flex-col gap-px text-xl text-stone-50 sm:text-3xl">
+          <h1 className="leading-none">
+            <AccentText>{person.name}</AccentText>
+          </h1>
 
-//           <div className="flex flex-col leading-[1.1] tracking-[0.02ch]">
-//             <h2>
-//               {person.jobTitle},{" "}
-//               <Link
-//                 href={person.company.url}
-//                 target="_blank"
-//                 className="inline-block capitalize underline decoration-1 underline-offset-[2.5px] transition hover:no-underline hover:opacity-30 sm:decoration-[1.5px] sm:underline-offset-[3.5px]"
-//               >
-//                 {person.company.name}
-//               </Link>
-//             </h2>
-//           </div>
-//         </div>
-//       </div>
-//     </StickyMobileWrapper>
-//   );
-// }
+          {person.work.map((work, index) => (
+            <div
+              className="flex flex-col leading-[1.1] tracking-[0.02ch]"
+              key={index}
+            >
+              <h2>
+                {work.title},{" "}
+                {work.url && (
+                  <Link
+                    href={work.url}
+                    target="_blank"
+                    className="inline-block capitalize underline decoration-1 underline-offset-[2.5px] transition hover:no-underline hover:opacity-30 sm:decoration-[1.5px] sm:underline-offset-[3.5px]"
+                  >
+                    {work.company}
+                  </Link>
+                )}
+              </h2>
+            </div>
+          ))}
+        </div>
+      </div>
+    </StickyMobileWrapper>
+  );
+}
