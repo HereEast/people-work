1ef397319e8387cccb6b9598e9cb490e8f8af8e6
Nastia Piven <nastiapiven@gmail.com>
2025-06-11 18:29:28 +0200

    fix: uncomment sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,47 +1,47 @@
-// import type { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
-// import { getQuestions } from "~/api-client/questions";
-// import { getPeople } from "~/api-client/people";
-// import { BASE_URL } from "~/utils/constants";
+import { getQuestions } from "~/api-client/questions";
+import { getPeople } from "~/api-client/people";
+import { BASE_URL } from "~/utils/constants";
 
-// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-//   const people = await getPeople();
-//   const questions = await getQuestions();
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  // const people = await getPeople();
+  // const questions = await getQuestions();
 
-//   const peopleSitemapData =
-//     people?.map((person) => {
-//       return {
-//         url: `${BASE_URL}/people/${person.slug}`,
-//         lastModified: new Date(),
-//       };
-//     }) || [];
+  // const peopleSitemapData =
+  //   people?.map((person) => {
+  //     return {
+  //       url: `${BASE_URL}/people/${person.slug}`,
+  //       lastModified: new Date(),
+  //     };
+  //   }) || [];
 
-//   const questionsSitemapData =
-//     questions?.map((question) => {
-//       return {
-//         url: `${BASE_URL}/questions/${question.slug}`,
-//         lastModified: new Date(),
-//       };
-//     }) || [];
+  // const questionsSitemapData =
+  //   questions?.map((question) => {
+  //     return {
+  //       url: `${BASE_URL}/questions/${question.slug}`,
+  //       lastModified: new Date(),
+  //     };
+  //   }) || [];
 
-//   return [
-//     {
-//       url: BASE_URL,
-//       lastModified: new Date(),
-//     },
-//     {
-//       url: `${BASE_URL}/about`,
-//       lastModified: new Date(),
-//     },
-//     {
-//       url: `${BASE_URL}/people`,
-//       lastModified: new Date(),
-//     },
-//     {
-//       url: `${BASE_URL}/questions`,
-//       lastModified: new Date(),
-//     },
-//     ...peopleSitemapData,
-//     ...questionsSitemapData,
-//   ];
-// }
+  return [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+    },
+    {
+      url: `${BASE_URL}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${BASE_URL}/people`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${BASE_URL}/questions`,
+      lastModified: new Date(),
+    },
+    // ...peopleSitemapData,
+    // ...questionsSitemapData,
+  ];
+}
