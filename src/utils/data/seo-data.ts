24bebb42732a783eb18ed21f1a488621385c1742
Nastia Piven <nastiapiven@gmail.com>
2025-06-12 21:19:39 +0200

    test: move code

--- src/utils/data/seo-data.ts
+++ src/utils/data/seo-data.ts
@@ -1,26 +1,26 @@
 import { BASE_URL } from "../constants";
 
-export const SEO_DATA = {
-  index: {
-    url: BASE_URL,
-    imageUrl: "/og-image.jpg",
-    title: "Job titles decoded. In a simple Q&A format.",
-    description:
-      "A small web hustle on a mission to demystify job titles and show real people behind them.",
-  },
-  person: {
-    title(name: string, title: string, company: string) {
-      return `${name}, ${title} at ${company}`;
-    },
-    description(name: string) {
-      return `Discover how ${name} approaches work, perspectives, and career growth. Explore unique insights and experiences in a simple Q&A format.`;
-    },
-  },
-  question: {
-    title(question: string) {
-      return `Q: ${question}`;
-    },
-    description:
-      "Explore diverse perspectives on this topic from professionals across industries.",
-  },
-};
+// export const SEO_DATA = {
+//   index: {
+//     url: BASE_URL,
+//     imageUrl: "/og-image.jpg",
+//     title: "Job titles decoded. In a simple Q&A format.",
+//     description:
+//       "A small web hustle on a mission to demystify job titles and show real people behind them.",
+//   },
+//   person: {
+//     title(name: string, title: string, company: string) {
+//       return `${name}, ${title} at ${company}`;
+//     },
+//     description(name: string) {
+//       return `Discover how ${name} approaches work, perspectives, and career growth. Explore unique insights and experiences in a simple Q&A format.`;
+//     },
+//   },
+//   question: {
+//     title(question: string) {
+//       return `Q: ${question}`;
+//     },
+//     description:
+//       "Explore diverse perspectives on this topic from professionals across industries.",
+//   },
+// };
--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,9 +1,35 @@
 import { Metadata } from "next";
 
 import { connectDB } from "~/_lib";
-import { SEO_DATA } from "./data/seo-data";
+// import { SEO_DATA } from "./data/seo-data";
 import { QuestionDB } from "~/models/Question";
 import { PersonDB } from "~/models/Person";
+import { BASE_URL } from "./constants";
+
+export const SEO_DATA = {
+  index: {
+    url: BASE_URL,
+    imageUrl: "/og-image.jpg",
+    title: "Job titles decoded. In a simple Q&A format.",
+    description:
+      "A small web hustle on a mission to demystify job titles and show real people behind them.",
+  },
+  person: {
+    title(name: string, title: string, company: string) {
+      return `${name}, ${title} at ${company}`;
+    },
+    description(name: string) {
+      return `Discover how ${name} approaches work, perspectives, and career growth. Explore unique insights and experiences in a simple Q&A format.`;
+    },
+  },
+  question: {
+    title(question: string) {
+      return `Q: ${question}`;
+    },
+    description:
+      "Explore diverse perspectives on this topic from professionals across industries.",
+  },
+};
 
 // Person metadata
 export async function generatePersonMetadata(slug: string) {
