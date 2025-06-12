fdfe6bd7b27fa1ef0712823d830c9370945332a0
Nastia Piven <nastiapiven@gmail.com>
2025-06-12 20:36:43 +0200

    refactor: update constants

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,15 +1,36 @@
 import { Metadata } from "next";
 
-import { connectDB } from "~/_lib";
-import { SEO_DATA } from "./data/seo-data";
-import { QuestionDB } from "~/models/Question";
-import { PersonDB } from "~/models/Person";
+import { getPerson, getQuestion } from "~/_lib";
+import { BASE_URL } from "./constants";
+
+export const SEO_DATA = {
+  index: {
+    url: BASE_URL,
+    imageUrl: "/og-image.jpg",
+    title: "Job titles decoded. In a simple Q&A format.",
+    description:
+      "A small web hustle on a mission to demystify job titles and show real people behind them.",
+  },
+  person: {
+    title(name: string, title: string, company: string) {
+      return `${name}, ${title} at ${company}`;
+    },
+    description(name: string) {
+      return `Discover how ${name} approaches work, perspectives, and career growth. Explore unique insights and experiences in a simple Q&A format.`;
+    },
+  },
+  question: {
+    title(question: string) {
+      return `Q: ${question}`;
+    },
+    description:
+      "Explore diverse perspectives on this topic from professionals across industries.",
+  },
+};
 
 // Person metadata
 export async function generatePersonMetadata(slug: string) {
-  await connectDB();
-
-  const person = await PersonDB.findOne({ slug }).exec();
+  const person = await getPerson(slug);
 
   if (person) {
     const title = SEO_DATA.person.title(
@@ -26,9 +47,7 @@ export async function generatePersonMetadata(slug: string) {
 
 // Question metadata
 export async function generateQuestionMetadata(slug: string) {
-  await connectDB();
-
-  const question = await QuestionDB.findOne({ slug, isActive: true }).exec();
+  const question = await getQuestion(slug);
 
   if (question) {
     const title = SEO_DATA.question.title(question.body);
