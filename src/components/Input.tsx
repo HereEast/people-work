a30ed4b7faddb37efb8df7e0a5ba6a1fced7137b
Nastia Piven <nastiapiven@gmail.com>
2025-06-17 22:25:22 +0200

    refactor: update input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,9 @@
 import { forwardRef } from "react";
-// import { FieldError } from "react-hook-form";
 
 import { cn } from "~/utils/handlers";
 
 interface InputProps {
   placeholder: string;
-  // error?: FieldError;
   name: string;
   disabled?: boolean;
   className?: string;
@@ -20,8 +18,6 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
             "h-16 w-full rounded-md border border-stone-400 bg-transparent px-5 pb-px text-xl outline-none placeholder:text-xl placeholder:text-stone-600/30 focus:border-stone-900 sm:h-20 sm:rounded-lg sm:text-3xl sm:placeholder:text-3xl",
             className,
           )}
-          // placeholder={placeholder}
-          // disabled={disabled}
           ref={ref}
           {...props}
         />
@@ -29,24 +25,5 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
     );
   },
 );
-
-// export const Input = forwardRef<HTMLInputElement, InputProps>(
-//   ({ placeholder, error, className = "", disabled = false, ...props }, ref) => {
-//     return (
-//       <div className="relative w-full">
-//         <input
-//           className={cn(
-//             "h-16 w-full rounded-md border border-stone-400 bg-transparent px-5 pb-px text-xl outline-none placeholder:text-xl placeholder:text-stone-600/30 focus:border-stone-900 sm:h-20 sm:rounded-lg sm:text-3xl sm:placeholder:text-3xl",
-//             className,
-//           )}
-//           placeholder={placeholder}
-//           disabled={disabled}
-//           ref={ref}
-//           {...props}
-//         />
-//       </div>
-//     );
-//   },
-// );
 
 Input.displayName = "Input";
