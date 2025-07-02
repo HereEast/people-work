7d225b7d5edc534aa1aa7d151e23eaca81d4d8c6
Nastia Piven <nastiapiven@gmail.com>
2025-07-02 18:38:19 +0200

    design: update questions list

---
+++ src/components/QuestionsList.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+
+import { QuestionTag } from "./QuestionTag";
+import { QuestionData } from "~/schemas";
+import { GoToPseudoButton } from "./ui";
+
+interface QuestionsListProps {
+  questions: QuestionData[];
+}
+
+export function QuestionsList({ questions }: QuestionsListProps) {
+  return (
+    <div>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem question={question} key={question.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// Question Item
+interface QuestionItemProps {
+  question: QuestionData;
+}
+
+function QuestionItem({ question }: QuestionItemProps) {
+  return (
+    <li className="group border-t border-stone-900/15 px-px last:border-b">
+      <Link
+        href={`/questions/${question.slug}`}
+        className="grid grid-cols-[1fr_auto] items-center gap-6 py-4 transition lg:grid-cols-2 lg:gap-4 lg:py-5"
+      >
+        <div className="lg:pr-10">
+          <h2 className="text-xl font-semibold leading-[120%] tracking-[-0.02ch] transition group-hover:opacity-30 sm:text-3xl lg:text-5xl lg:leading-[100%]">
+            {question.body}
+          </h2>
+        </div>
+
+        <div className="grid w-full grid-cols-[1fr_auto] lg:ml-auto lg:min-w-[380px] lg:max-w-[480px] lg:gap-4">
+          <div className="hidden lg:block">
+            <QuestionTag
+              slug={question.slug}
+              className="group-hover:opacity-45"
+            />
+          </div>
+
+          <div className="">
+            <GoToPseudoButton className="group-hover:bg-stone-600/15" />
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
+}
