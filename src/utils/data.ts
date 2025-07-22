ea2ff4eb2548dd2c41fa24813366e0bce9f13497
Nastia Piven <nastiapiven@gmail.com>
2025-07-22 10:15:43 +0200

    add: add Hugo Barbera

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,19 +1,19 @@
 export const FEATURED = [
   {
-    id: 9,
-    slug: "bartek-hlawka",
-  },
-  {
-    id: 5,
-    slug: "alla-aloe",
-  },
-  {
-    id: 8,
-    slug: "kate-rovba",
+    id: 10,
+    slug: "hugo-barbera",
   },
   {
     id: 7,
     slug: "gev-marotz",
+  },
+  {
+    id: 9,
+    slug: "bartek-hlawka",
+  },
+  {
+    id: 8,
+    slug: "kate-rovba",
   },
 ];
 
@@ -57,5 +57,9 @@ export const ALL_SLUGS = [
   {
     id: 9,
     slug: "bartek-hlawka",
+  },
+  {
+    id: 10,
+    slug: "hugo-barbera",
   },
 ];
